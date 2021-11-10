a83d83a75b69fb9e145ff17ddbaeae3935920637
Deny Pătrașcu <denypatrascu@gmail.com>
2021-11-10 18:38:02 +0200

    Add proxy methods to any array from the model

--- base/libs/bindableModel.js
+++ base/libs/bindableModel.js
@@ -615,6 +615,26 @@ class PskBindableModel {
                                     return target[prop];
                                 }
                         }
+                    } else {
+                      // TODO: under supervision
+                      switch (prop) {
+                        case "onChange":
+                          return onChange;
+                        case "offChange":
+                          return offChange;
+                        case "notify":
+                          return notify;
+                        case "getChainValue":
+                          return getChainValue;
+                        case "setChainValue":
+                          return setChainValue;
+                        case "cleanReferencedChangeCallbacks":
+                          return cleanReferencedChangeCallbacks;
+                        default:
+                          if(PROXY_ROOT_METHODS.includes(prop)) {
+                            return target[prop];
+                          }
+                      }
                     }
 
                     if (prop === "__isProxy") {
